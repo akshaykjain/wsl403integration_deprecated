72baf57682c1cb80653cfc8856eebb93ec9909df
User One <user1@somewhere.com>
2021-10-06 11:17:51 +0000

    adding a basic timer R script

---
+++ assets/rstudio/basic_timer.R
@@ -0,0 +1,13 @@
+seconds = 15
+current_time = Sys.time()
+current_date = Sys.Date()
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+print("Starting Timer at:")
+print(paste0("Date: ", current_date))
+print(paste0("Time: ", current_time))
+Sys.sleep(seconds)
+print("Stopped Timer at:")
+print(paste0("Date: ", current_date))
+print(paste0("Time: ", current_time))
+print(paste0("after running for : ", seconds))
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
